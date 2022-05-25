fe8c2a43ed829cc4999ba6ada04e2af020423d47
Mónica Szewczuk <mvszewczuk@gmail.com>
2022-05-25 19:44:55 -0300

    add ejs

---
+++ ejs/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+const Container = require('./container');
+const fileName = new Container ("productos.txt");
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+//Vista de todos los productos
+app.get('/productos', (req, res) => {
+    const getProducts = async () => {
+        const products = await fileName.getAll();
+        res.render('pages/main', {products})
+    };
+    getProducts();
+});
+
+//Para redirigir al formulario de carga de productos
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/input.html');
+})
+
+
+//Para agregar un producto
+app.post('/productos', (req, res) => {
+    const newProduct = req.body;
+    const getProducts = async () => {
+        const newId = await fileName.save(newProduct);
+        res.sendFile(__dirname + '/public/input.html');
+    };
+    getProducts();
+});
+
+app.listen(8080, () => {
+    console.log("escuchando desafio 10 ejs");
+});
+
+
+
